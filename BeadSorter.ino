8ea588a06998883df900be23369459faa683f5ab
jeroenweij88@gmail.com <jeroen.weij@fox-it.com>
2020-12-29 16:27:12 +0100

    add ssprintf

--- BeadSorter.ino
+++ BeadSorter.ino
@@ -1,10 +1,22 @@
 #include "TCS.h"
 
-#define defdelta 10
+#define defdelta  10
+#define PRINT_BUF 100
+
+static void ssprintf(char* fmt, ...)
+{
+    static char buf[PRINT_BUF];
+    va_list va;
+    va_start(va, fmt);
+    vsnprintf(buf, PRINT_BUF, fmt, va);
+    va_end(va);
+
+    Serial.print(buf);
+}
 
 static const Color colors[] =
 {
-    //R B  W  G  D
+    // R B  W  G  D
     {0, 0, 0, 0, defdelta},
     {0, 0, 0, 0, defdelta}
 };
@@ -37,5 +49,7 @@ void loop()
     Serial.print(c.blue);
     Serial.print(" W = ");
     Serial.println(c.white);
+    ssprintf("%04d, %04d, %04d, %04d", c.red, c.blue, c.white, c.green);
+    Serial.println();
     delay(250);
 }
